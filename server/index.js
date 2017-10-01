c5f925f6e5c81798a0994345e90fedc4f1701cf3
Angela Lin <tigclaw@gmail.com>
2017-10-01 18:26:55 -0400

    Edit server and index.html

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../client')));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('App server listening on port 3000!');
 });
